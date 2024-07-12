1100578346cb0757527c3bf3a5901c986935c077
rgdevment <mhidalgog@outlook.com>
2024-07-12 11:00:48 -0400

    refactor: error with i18n

--- src/indicators/validators/indicator-parse.pipe.ts
+++ src/indicators/validators/indicator-parse.pipe.ts
@@ -1,15 +1,17 @@
 import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
 import { IndicatorEnum } from '../indicator.enum';
+import { I18nService } from "nestjs-i18n";
 
 @Injectable()
-export class IndicatorParsePipe
-  implements PipeTransform<string, IndicatorEnum>
-{
+export class IndicatorParsePipe implements PipeTransform<string, IndicatorEnum> {
+  constructor(private readonly i18n: I18nService) {}
+
   transform(value: string): IndicatorEnum {
     const enumValue = value.toUpperCase() as unknown as IndicatorEnum;
 
     if (!Object.values(IndicatorEnum).includes(enumValue)) {
-      throw new BadRequestException(`${value} is not a valid indicator value`);
+      const message = this.i18n.t('globals.invalidIndicatorValue', { args: { value } });
+      throw new BadRequestException(message);
     }
 
     return enumValue;

f606b63302916fd98f489485c05d7dc41237f77e
rgdevment <mhidalgog@outlook.com>
2024-07-12 19:40:38 -0400

    feat: configuration to load .env and .env.local
    - Added dotenv package to load environment variables.
    - Configured config.ts to load .env first and then .env.local, ensuring .env.local overwrites .env variables if they exist.
    - Updated AppModule to use environment variables for MongoDB connection.
    - This allows for flexible and customizable environment configurations, especially useful for local development overrides.

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,10 @@
 import * as dotenv from 'dotenv';
+import * as fs from 'node:fs';
 
-dotenv.config();
+if (fs.existsSync('.env')) {
+  dotenv.config({ path: '.env' });
+}
+
+if (fs.existsSync('.env.local')) {
+  dotenv.config({ path: '.env.local' });
+}
